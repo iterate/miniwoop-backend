4e6bc456029694b3624ddb52a303126a58c68449
Sindre Johansen <sindre@iterate.no>
2017-06-22 07:59:48 +0200

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate unicase;
+extern crate uuid;
+extern crate chrono;
 
 use std::sync::{Arc, Mutex};
 use std::vec::Vec;
+use std::collections::HashMap;
 
 
 use futures::future;
@@ -22,10 +25,34 @@ use hyper::header;
 use hyper::Headers;
 use hyper::server::{Http, Service, Request, Response};
 use serde::ser::Serialize;
+use uuid::Uuid;
+use chrono::{DateTime, UTC};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Clone)]
 struct Message {
+    id: Uuid,
     text: String,
+    woops: i32,
+    created: DateTime<UTC>,
+    user: String,
+}
+
+impl From<MessageIncoming> for Message {
+    fn from(msg: MessageIncoming) -> Message {
+        Message {
+            text: msg.text,
+            woops: 0,
+            id: Uuid::new_v4(),
+            created: UTC::now(),
+            user: msg.user.unwrap_or_else(|| "Anonym".to_string()),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct MessageIncoming {
+    text: String,
+    user: Option<String>,
 }
 
 fn json_response<T: Serialize>(data: &T) -> Response {
@@ -40,7 +67,7 @@ fn json_response<T: Serialize>(data: &T) -> Response {
 }
 
 struct MiniWoopServer {
-    messages: Arc<Mutex<Vec<Message>>>,
+    messages: Arc<Mutex<HashMap<Uuid, Message>>>,
 }
 
 impl Service for MiniWoopServer {
@@ -71,18 +98,21 @@ impl Service for MiniWoopServer {
         match (req.method(), parts_slice) {
             (&Get, &[]) => {
                 futures::future::ok(Response::new()
-                    .with_body("POST / GET /messages")).boxed()
+                    .with_body("Try /messages")).boxed()
             },
             (&Get, &["messages"]) => {
+                let messages = self.messages.lock().unwrap();
+                let mut messages_vec: Vec<_> = messages.values().collect();
+                messages_vec.sort_by_key(|m| m.created);
                 future::ok(
-                    json_response(&self.messages.lock().unwrap() as &Vec<Message>)
+                    json_response(&messages_vec)
                         .with_headers(default_headers)
                 ).boxed()
             },
             (&Post, &["messages"]) => {
                 let messages = self.messages.clone();
                 req.body().concat2().map(move |body| {
-                    let body: Message = match serde_json::from_slice(&body) {
+                    let body: MessageIncoming = match serde_json::from_slice(&body) {
                         Ok(body) => body,
                         Err(_) => {
                             return Response::new()
@@ -92,18 +122,38 @@ impl Service for MiniWoopServer {
                         }
                     };
 
-                    messages.lock().unwrap().push(body);
-                    let msgs = &messages.lock().unwrap();
+                    let message: Message = body.into();
 
-                    match serde_json::to_vec(msgs as &Vec<Message>) {
-                        Ok(serialized) => Response::new()
-                            .with_body(serialized.clone()),
-                        Err(_) => Response::new()
-                            .with_header(header::ContentType(mime::APPLICATION_JSON))
-                            .with_status(StatusCode::InternalServerError)
-                            .with_body("Could not encode JSON")
-                    }.with_headers(default_headers)
+                    messages.lock().unwrap().insert(message.id, message.clone());
+
+                    json_response(&message)
+                        .with_headers(default_headers)
                 }).boxed()
+            },
+            (&Post, &["messages", message_id, "woop"]) => {
+                let messages = self.messages.clone();
+                let message_id:Uuid = match message_id.parse() {
+                    Ok(id) => id,
+                    Err(_) => {
+                        return futures::future::ok(Response::new()
+                            .with_headers(default_headers)
+                            .with_status(StatusCode::NotFound)).boxed()
+                    }
+                };
+
+                let mut messages = messages.lock().unwrap();
+
+                let message = match messages.get_mut(&message_id) {
+                    Some(message) => message,
+                    None => return futures::future::ok(Response::new()
+                        .with_headers(default_headers)
+                        .with_status(StatusCode::NotFound)).boxed(),
+                };
+
+                message.woops += 1;
+
+                future::ok(json_response(&message)
+                        .with_headers(default_headers)).boxed()
             },
             _ => {
                 futures::future::ok(Response::new()
@@ -120,7 +170,7 @@ fn main() {
     pretty_env_logger::init().unwrap();
     let addr = "0.0.0.0:5000".parse().unwrap();
 
-    let messages = Arc::new(Mutex::new(Vec::new()));
+    let messages = Arc::new(Mutex::new(HashMap::new()));
 
     let server = Http::new().bind(&addr, move || Ok(MiniWoopServer {
         messages: messages.clone(),
